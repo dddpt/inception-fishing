f91470ef325d3f4b0fb121e75dad3664777d5b1d
dddpt <didier.dupertuis@epfl.ch>
2021-10-25 16:55:32 +0200

    possible to change NE tagname

--- inception_fishing.py
+++ inception_fishing.py
@@ -6,6 +6,8 @@ from lxml import etree, objectify
 
 # %%
 
+DEFAULT_NAMED_ENTITY_TAGSET_TAG_STR = '<type2:TagsetDescription xmi:id="8999" sofa="1" begin="0" end="0" layer="de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity" name="Named Entity tags" input="false"/>'
+
 
 def get_attributes_string(class_name, object_dict):
     """Unimportant utility function to format __str__() and __repr()"""
@@ -39,37 +41,37 @@ class Document:
         self.text = text
         
     def entity_fishing_get_text_from_corpus_folder(self, corpus_folder):
-        with open(path.join(corpus_folder, self.name)) as f:
+        text_file_path = path.join(corpus_folder, self.name) if corpus_folder else self.name
+        with open(text_file_path) as f:
                 self.text = f.read()
-                print("Document, corpus_folder: ", corpus_folder)
                 return self.text
     def __repr__(self):
         return get_attributes_string("Document",self.__dict__)
 
-    def inception_to_xml_string(self, force_single_sentence=False, named_entities_xmi_ids_start = 9000):
+    def inception_to_xml_string(self, force_single_sentence=False, named_entities_xmi_ids_start = 9000, tagset_tag_str=DEFAULT_NAMED_ENTITY_TAGSET_TAG_STR, **named_entity_to_tag_kwargs):
         """Returns a valid inception input file content in UIMA CAS XMI (XML 1.1) format
         
         Note: replaces " characters in text wth ', to simplify handling of XML.
         force_single_sentence=True forces the whole document text to be considered as a single sentence by inception,
         useful when text contains non-sentence-inducing dots (such as abbreviation dots in the DHS)
         """
-        named_entities_str ="\n            ".join(ne.inception_to_tag_string(named_entities_xmi_ids_start+i) for i, ne in enumerate(self.named_entities))
+        named_entities_str ="\n            ".join(ne.inception_to_tag_string(named_entities_xmi_ids_start+i, **named_entity_to_tag_kwargs) for i, ne in enumerate(self.named_entities))
         force_single_sentence_str = f'\n            <type4:Sentence xmi:id="8998" sofa="1" begin="0" end="{len(self.text)}"/>' if force_single_sentence else ""
         return f'''
         <?xml version="1.1" encoding="UTF-8"?>
         <xmi:XMI xmlns:pos="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos.ecore" xmlns:tcas="http:///uima/tcas.ecore" xmlns:xmi="http://www.omg.org/XMI" xmlns:cas="http:///uima/cas.ecore" xmlns:tweet="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos/tweet.ecore" xmlns:morph="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/morph.ecore" xmlns:dependency="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/dependency.ecore" xmlns:type5="http:///de/tudarmstadt/ukp/dkpro/core/api/semantics/type.ecore" xmlns:type8="http:///de/tudarmstadt/ukp/dkpro/core/api/transform/type.ecore" xmlns:type7="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type.ecore" xmlns:type2="http:///de/tudarmstadt/ukp/dkpro/core/api/metadata/type.ecore" xmlns:type9="http:///org/dkpro/core/api/xml/type.ecore" xmlns:type3="http:///de/tudarmstadt/ukp/dkpro/core/api/ner/type.ecore" xmlns:type4="http:///de/tudarmstadt/ukp/dkpro/core/api/segmentation/type.ecore" xmlns:type="http:///de/tudarmstadt/ukp/dkpro/core/api/coref/type.ecore" xmlns:type6="http:///de/tudarmstadt/ukp/dkpro/core/api/structure/type.ecore" xmlns:constituent="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/constituent.ecore" xmlns:chunk="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/chunk.ecore" xmlns:custom="http:///webanno/custom.ecore" xmi:version="2.0">
             <cas:NULL xmi:id="0"/>
             {named_entities_str}{force_single_sentence_str}
-            <type2:TagsetDescription xmi:id="8999" sofa="1" begin="0" end="0" layer="de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity" name="Named Entity tags" input="false"/>
+            {tagset_tag_str}
             <cas:Sofa xmi:id="1" sofaNum="1" sofaID="_InitialView" mimeType="text" sofaString="{self.text.replace('"', "'")}"/>
             <cas:View sofa="1" members="{("8998 " if force_single_sentence else "")}8999 {" ".join(str(named_entities_xmi_ids_start+i) for i, ne in enumerate(self.named_entities))}"/>
         </xmi:XMI>
         '''.replace("\n    ","\n").strip()
-    def inception_to_xml_file(self, folder="./", filename=None, force_single_sentence=False, named_entities_xmi_ids_start = 9000):
+    def inception_to_xml_file(self, folder="./", filename=None, force_single_sentence=False, named_entities_xmi_ids_start = 9000, **named_entity_to_tag_kwargs):
         if not filename:
             filename=self.name
         with open(path.join(folder,filename), "w") as outfile:
-            outfile.write(self.inception_to_xml_string(force_single_sentence, named_entities_xmi_ids_start))
+            outfile.write(self.inception_to_xml_string(force_single_sentence, named_entities_xmi_ids_start, **named_entity_to_tag_kwargs))
     @staticmethod
     def entity_fishing_from_tag(ef_xml_document_tag, corpus_folder = None):
         named_entities_tags = ef_xml_document_tag.findall("annotation")
@@ -78,7 +80,6 @@ class Document:
             [NamedEntity.entity_fishing_from_tag(t) for t in named_entities_tags]
         )
         if corpus_folder:
-            print("Document, corpus_folder: ", corpus_folder)
             doc.entity_fishing_get_text_from_corpus_folder(corpus_folder)
         return doc
 
@@ -95,9 +96,12 @@ class NamedEntity:
     @length.setter
     def set_length(self, new_length):
         self.end = self.start+new_length
-    def inception_to_tag_string(self, xmi_id):
-        """Returns a valid <type3:NamedEntity/> tag string for inception's UIMA CAS XMI (XML 1.1) format"""
-        return f'<type3:NamedEntity xmi:id="{xmi_id}" sofa="1" begin="{self.start}" end="{self.end}" identifier="{self.wikidata_entity_url}"/>'
+    def inception_to_tag_string(self, xmi_id, tag_name="type3:NamedEntity", identifier_attribute_name="identifier"):
+        """Returns a valid <type3:NamedEntity/> tag string for inception's UIMA CAS XMI (XML 1.1) format
+
+        Tag & attribute name can be changed
+        """
+        return f'<{tag_name} xmi:id="{xmi_id}" sofa="1" begin="{self.start}" end="{self.end}" {identifier_attribute_name}="{self.wikidata_entity_url}"/>'
     def __repr__(self):
         return get_attributes_string("NamedEntity",self.__dict__)
     @staticmethod
@@ -144,15 +148,18 @@ doc.inception_to_xml_file()
 
 # %%
 
-entity_fishing_corpus_folder = "../entity-fishing/data/corpus/corpus-long/dhs-training-de/RawText/"
+entity_fishing_corpus_folder = "../entity-fishing/data/corpus/corpus-long/dhs-training-de/"
+entity_fishing_annotation_output_file = path.join(entity_fishing_corpus_folder,"dhs-training-de.xml")
+entity_fishing_corpus_rawtext_folder = path.join(entity_fishing_corpus_folder, "RawText/")
 
-with open("entity-fishing-format-example.xml") as entity_fishing_example_file:
-    entity_fishing_xml_root = etree.parse(entity_fishing_example_file).getroot()
-    corpus = Corpus.entity_fishing_from_tag_and_corpus(entity_fishing_xml_root, entity_fishing_corpus_folder)
+with open(entity_fishing_annotation_output_file) as entity_fishing_xml_file:
+    entity_fishing_xml_root = etree.parse(entity_fishing_xml_file).getroot()
+    corpus = Corpus.entity_fishing_from_tag_and_corpus(entity_fishing_xml_root, entity_fishing_corpus_rawtext_folder)
 # %%
 
+entity_fishing_tag_str = '<type2:TagsetDescription xmi:id="1780" sofa="1" begin="0" end="0" layer="webanno.custom.Entityfishinglayer" name="Grobid-NER" input="false"/>'
 
 inception_import_folder = "../inception-import-xml/"
 for d in corpus.documents:
-    d.inception_to_xml_file(inception_import_folder, force_single_sentence=True)
+    d.inception_to_xml_file(inception_import_folder, force_single_sentence=True, tag_name="custom:Entityfishinglayer", identifier_attribute_name="wikidataidentifier")
 # %%
