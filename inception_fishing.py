a5fb42e8c1db5b75b17ed53cb56e7ef8b676a36c
dddpt <didier.dupertuis@epfl.ch>
2021-11-03 19:29:15 +0100

    classes reordering for typing (no content change)

--- inception_fishing.py
+++ inception_fishing.py
@@ -18,34 +18,70 @@ def get_attributes_string(class_name, object_dict):
 
 # %%
 
+wikidata_entity_base_url = "http://www.wikidata.org/entity/"
 
-class Corpus:
-    def __init__(self, name, documents):
-        self.name = name
-        self.documents = documents
-    @staticmethod
-    def entity_fishing_from_tag_and_corpus(ef_xml_root_tag, corpus_folder = None):
-        """Returns a Corpus object from a lxml.etree tag (the root of an EF evaluation XML output) and the EF corpus folder"""
-        name = ef_xml_root_tag.tag.replace(".entityAnnotation", "")
-        document_tags = ef_xml_root_tag.findall("document")
-        return Corpus(name, [Document.entity_fishing_from_tag(t, corpus_folder) for t in document_tags])
+
+class Annotation:
+    def __init__(self, start, end, wikidata_entity_url=None, grobid_tag=None):
+        """Creates Annotation, end is non-inclusive"""
+        self.start = start
+        self.end = end
+        self.wikidata_entity_url = wikidata_entity_url
+        self.grobid_tag = grobid_tag
+    @property
+    def length(self):
+        return self.end-self.start
+    @length.setter
+    def set_length(self, new_length):
+        self.end = self.start+new_length
+    def inception_to_tag_string(self, xmi_id, tag_name="type3:NamedEntity", identifier_attribute_name="identifier"):
+        """Returns a valid <type3:NamedEntity/> tag string for inception's UIMA CAS XMI (XML 1.1) format
+
+        Tag & attribute name can be changed
+        """
+        return f'<{tag_name} xmi:id="{xmi_id}" sofa="1" begin="{self.start}" end="{self.end}" {identifier_attribute_name}="{self.wikidata_entity_url}"/>'
     def __repr__(self):
-        return get_attributes_string(
-            "Corpus",
-            {"name": self.name,
-            "documents": [d.name for d in self.documents]}
-        )
+        return get_attributes_string("Annotation",self.__dict__)
     @staticmethod
-    def inception_from_directory(name, dir_path, inception_user_name, **document_inception_from_file_kwargs):
-        documents_directories = listdir(dir_path)
-        documents = [
-            Document.inception_from_file(
-                path.join(dir_path,dd,inception_user_name+".xmi"),
-                dd,
-                **document_inception_from_file_kwargs
-            ) for dd in documents_directories
-        ]
-        return Corpus(name, documents)
+    def entity_fishing_from_tag(ef_xml_annotation_tag):
+        """
+        
+        <annotation>
+			<mention>14.1</mention>
+			<wikiName>14 und 1 endlos</wikiName>
+			<wikidataId>Q7468888</wikidataId>
+			<wikipediaId>3677337</wikipediaId>
+			<offset>0</offset>
+			<length>4</length>
+		</annotation>
+        """
+        offset = int(ef_xml_annotation_tag.find("offset").text)
+        length = int(ef_xml_annotation_tag.find("length").text)
+        wikidata_id = ef_xml_annotation_tag.find("wikidataId").text
+        return Annotation(offset, offset+length, wikidata_entity_base_url+wikidata_id)
+    @staticmethod
+    def inception_from_tag_string(tag_string, identifier_attribute_name="identifier", grobid_tag_attribute_name="entityfishingtag"):
+        """
+    
+        <custom:Entityfishinglayer xmi:id="3726" sofa="1" begin="224" end="244" entityfishingtag="INSTALLATION" wikidataidentifier="http://www.wikidata.org/entity/Q2971666"/>
+        """
+        offset_match = inception_being_regex.search(tag_string)
+        end_match = inception_end_regex.search(tag_string)
+        if (not offset_match) or (not end_match):
+            raise Exception(f"Annotation.inception_from_tag_string() missing begin or end attribute in tag: {tag_string}")
+        offset = offset_match.group(1)
+        end = end_match.group(1)
+
+        identifier_match = re.search(identifier_attribute_name+r'="(.+?)"', tag_string)
+        identifier = identifier_match.group(1) if identifier_match else None
+
+        grobid_tag_match = re.search(grobid_tag_attribute_name+r'="(.+?)"', tag_string)
+        grobid_tag = grobid_tag_match.group(1) if grobid_tag_match else None
+
+
+        return Annotation(offset, end, identifier, grobid_tag)
+        
+# %%
 
 
 class Document:
@@ -153,66 +189,35 @@ class Document:
                 document_name = file_path
             return Document.inception_from_string(document_name, document_string, **inception_from_string_kwargs)
 
-wikidata_entity_base_url = "http://www.wikidata.org/entity/"
 
+# %%
 
-class Annotation:
-    def __init__(self, start, end, wikidata_entity_url=None, grobid_tag=None):
-        """Creates Annotation, end is non-inclusive"""
-        self.start = start
-        self.end = end
-        self.wikidata_entity_url = wikidata_entity_url
-        self.grobid_tag = grobid_tag
-    @property
-    def length(self):
-        return self.end-self.start
-    @length.setter
-    def set_length(self, new_length):
-        self.end = self.start+new_length
-    def inception_to_tag_string(self, xmi_id, tag_name="type3:NamedEntity", identifier_attribute_name="identifier"):
-        """Returns a valid <type3:NamedEntity/> tag string for inception's UIMA CAS XMI (XML 1.1) format
-
-        Tag & attribute name can be changed
-        """
-        return f'<{tag_name} xmi:id="{xmi_id}" sofa="1" begin="{self.start}" end="{self.end}" {identifier_attribute_name}="{self.wikidata_entity_url}"/>'
+class Corpus:
+    def __init__(self, name, documents):
+        self.name = name
+        self.documents = documents
+    @staticmethod
+    def entity_fishing_from_tag_and_corpus(ef_xml_root_tag, corpus_folder = None):
+        """Returns a Corpus object from a lxml.etree tag (the root of an EF evaluation XML output) and the EF corpus folder"""
+        name = ef_xml_root_tag.tag.replace(".entityAnnotation", "")
+        document_tags = ef_xml_root_tag.findall("document")
+        return Corpus(name, [Document.entity_fishing_from_tag(t, corpus_folder) for t in document_tags])
     def __repr__(self):
-        return get_attributes_string("Annotation",self.__dict__)
+        return get_attributes_string(
+            "Corpus",
+            {"name": self.name,
+            "documents": [d.name for d in self.documents]}
+        )
     @staticmethod
-    def entity_fishing_from_tag(ef_xml_annotation_tag):
-        """
-        
-        <annotation>
-			<mention>14.1</mention>
-			<wikiName>14 und 1 endlos</wikiName>
-			<wikidataId>Q7468888</wikidataId>
-			<wikipediaId>3677337</wikipediaId>
-			<offset>0</offset>
-			<length>4</length>
-		</annotation>
-        """
-        offset = int(ef_xml_annotation_tag.find("offset").text)
-        length = int(ef_xml_annotation_tag.find("length").text)
-        wikidata_id = ef_xml_annotation_tag.find("wikidataId").text
-        return Annotation(offset, offset+length, wikidata_entity_base_url+wikidata_id)
-    @staticmethod
-    def inception_from_tag_string(tag_string, identifier_attribute_name="identifier", grobid_tag_attribute_name="entityfishingtag"):
-        """
-    
-        <custom:Entityfishinglayer xmi:id="3726" sofa="1" begin="224" end="244" entityfishingtag="INSTALLATION" wikidataidentifier="http://www.wikidata.org/entity/Q2971666"/>
-        """
-        offset_match = inception_being_regex.search(tag_string)
-        end_match = inception_end_regex.search(tag_string)
-        if (not offset_match) or (not end_match):
-            raise Exception(f"Annotation.inception_from_tag_string() missing begin or end attribute in tag: {tag_string}")
-        offset = offset_match.group(1)
-        end = end_match.group(1)
+    def inception_from_directory(name, dir_path, inception_user_name, **document_inception_from_file_kwargs):
+        documents_directories = listdir(dir_path)
+        documents = [
+            Document.inception_from_file(
+                path.join(dir_path,dd,inception_user_name+".xmi"),
+                dd,
+                **document_inception_from_file_kwargs
+            ) for dd in documents_directories
+        ]
+        return Corpus(name, documents)
 
-        identifier_match = re.search(identifier_attribute_name+r'="(.+?)"', tag_string)
-        identifier = identifier_match.group(1) if identifier_match else None
-
-        grobid_tag_match = re.search(grobid_tag_attribute_name+r'="(.+?)"', tag_string)
-        grobid_tag = grobid_tag_match.group(1) if grobid_tag_match else None
-
-
-        return Annotation(offset, end, identifier, grobid_tag)
-        
+# %%
