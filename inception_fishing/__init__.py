3073228d9f86123d4cc1e9a01b8e306953361030
dddpt <didier.dupertuis@epfl.ch>
2021-12-14 11:51:57 +0100

    bug fix __init__.py

--- inception_fishing/__init__.py
+++ inception_fishing/__init__.py
@@ -1,5 +1,9 @@
-from . import entity_fishing
-from . import inception
-from . import clef_hipe_scorer
-from . import dhs_article
-from . import grobid_ner
+from . import Annotation
+from . import Document
+from . import Corpus
+
+from .import_export import entity_fishing
+from .import_export import inception
+from .import_export import clef_hipe_scorer
+from .import_export import dhs_article
+from .import_export import grobid_ner
